3423c1cb59f7ed608f1fd24cb476cffb647c5d7a
Diarmuid O'Neill <diarmuid.oneill@gmail.com>
2020-12-01 09:21:11 +0000

    Use with to open file

--- day1/day1pt1.py
+++ day1/day1pt1.py
@@ -3,10 +3,10 @@
 from itertools import combinations
 import sys
 
-f = open(sys.argv[1])
-input = [int(num) for num in f.readlines()]
-pairs = combinations(input,2)
-for i in pairs:
-    if i[0] + i[1] == 2020:
-        print(i[0] * i[1])
-        break
+with open(sys.argv[1]) as f:
+    input = [int(num) for num in f.readlines()]
+    pairs = combinations(input,2)
+    for i in pairs:
+        if i[0] + i[1] == 2020:
+            print(i[0] * i[1])
+            break
