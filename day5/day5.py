cb9b800c6bc757efcab7b2969906772a5f67f0a9
Diarmuid O'Neill <diarmuid.oneill@gmail.com>
2020-12-05 13:40:44 +0000

    Day 5

---
+++ day5/day5.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+import string
+
+def seatid_to_num(seat):
+    return int(''.join(['1' if digit == 'B' or digit == 'R' else '0' for digit in seat]), 2)
+
+def num_to_row(num):
+    return (num &  ~7) >> 3
+
+def get_seatid(seat):
+    row = num_to_row(seat)
+    col = seat & 7
+    return (row * 8) + col
+
+
+with open(sys.argv[1]) as f:
+    input = [seatid_to_num(line.strip()) for line in f.readlines()]
+
+    #Part 1
+    seat_ids = [get_seatid(seat_id) for seat_id in input]
+    print(max(seat_ids))
+
+    #Part 2
+    seat_nums = [seat_id for seat_id in input]
+    print(set(range(min(seat_nums), max(seat_nums) + 1)) - set(seat_nums))
