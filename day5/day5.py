9b0a8fa587c4b5b0367c86a80b0a975ac9de12cc
Diarmuid O'Neill <diarmuid.oneill@gmail.com>
2020-12-05 14:15:06 +0000

    Day 5

--- day5/day5.py
+++ day5/day5.py
@@ -6,18 +6,13 @@ import string
 def seatid_to_num(seat):
     return int(''.join(['1' if digit == 'B' or digit == 'R' else '0' for digit in seat]), 2)
 
-def get_seatid(seat):
-    row = (seat &  ~7) >> 3
-    col = seat & 7
-    return (row * 8) + col
-
 with open(sys.argv[1]) as f:
     input = [seatid_to_num(line.strip()) for line in f.readlines()]
 
     #Part 1
-    seat_ids = [get_seatid(seat_id) for seat_id in input]
+    seat_ids = [seat_id for seat_id in input]
     print(max(seat_ids))
 
     #Part 2
     seat_nums = [seat_id for seat_id in input]
-    print(set(range(min(seat_nums), max(seat_nums) + 1)) - set(seat_nums))
+    print((set(range(min(seat_nums), max(seat_nums) + 1)) - set(seat_nums)).pop())
