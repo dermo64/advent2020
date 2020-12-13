8bac55bded4f762acb37b154dff62800dcf6763a
Diarmuid O'Neill <diarmuid.oneill@gmail.com>
2020-12-13 17:31:00 +0000

    Day 13

--- day14/day14.py
+++ day14/day14.py
@@ -15,14 +15,15 @@ bus_tuples = [(int(bus), i) for i, bus in enumerate(busses) if bus != "x"]
 step = int(bus_tuples[0][0])
 i = 0
 solved = 0
-while solved < len(bus_tuples) - 1:
+while solved < len(bus_tuples)-1:
     i += step
     for num, bus in enumerate(bus_tuples):
-        if (i + bus[1]) % bus[0] != 0:
-            break
-        if solved < num:
+        if (i + bus[1]) % bus[0] == 0:
+            if solved < num:
                 solved += 1
                 step = step * bus[0]
+        else:
+            break
 print(i)
 
 
